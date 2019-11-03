98d5b071e22499e49f650b1f662c9fde7371c806
SergeyAI <sergey.sai71@gmail.com>
2019-11-03 23:09:36 +0500

    update js

--- tscolorballs.js
+++ tscolorballs.js
@@ -3,7 +3,6 @@ class Board {
     constructor(width, height) {
         this.width = width;
         this.height = height;
-        this.type_count = 4;
         this.tiles = Array();
         this.resetBoard();
     }
@@ -11,22 +10,23 @@ class Board {
         for (let n = 0; n < this.width; n++) {
             this.tiles[n] = new Array(this.height);
             for (let m = 0; m < this.height; m++) {
-                this.tiles[n][m] = this.randomInteger(1, this.type_count);
+                this.tiles[n][m] = this.randomInteger(1, Board.TILE_TYPES_COUNT);
             }
         }
-    }
-    setTileState(x, y, tileState) {
-        this.tiles[x][y] = tileState;
-    }
-    getTileState(x, y) {
-        return this.tiles[x][y];
     }
     randomInteger(min, max) {
         let rand = min + Math.random() * (max + 1 - min);
         rand = Math.floor(rand);
         return rand;
     }
+    getTileState(x, y) {
+        return this.tiles[x][y];
+    }
+    setTileState(x, y, tileState) {
+        this.tiles[x][y] = tileState;
+    }
 }
+Board.TILE_TYPES_COUNT = 4;
 class Engine {
     constructor() {
         this.situation = Situation.CLEAR_SCREEN;
@@ -39,7 +39,7 @@ class Engine {
         let engine = this;
         this.canvas.addEventListener("mousedown", function (event) { engine.onClick(event); });
         this.readHighScoreFromStorage();
-        engine.onStep();
+        this.onStep();
     }
     readHighScoreFromStorage() {
         try {
@@ -66,21 +66,17 @@ class Engine {
     initGame() {
         this.score = 0;
         this.board.resetBoard();
-        this.situation = Situation.CLEAR_SCREEN;
+        this.situation = Situation.GAME;
     }
     onStep() {
         let engine = this;
-        setTimeout(function () { engine.onStep(); }, Engine.TICK_MILLISECONDS);
         let ctx = this.canvas
             .getContext("2d");
         switch (this.situation) {
             case Situation.ANIMATION:
+                setTimeout(function () { engine.onStep(); }, Engine.TICK_MILLISECONDS);
                 this.processGravity();
                 this.paintGame(ctx);
-                break;
-            case Situation.CLEAR_SCREEN:
-                this.clearScreen(ctx);
-                this.situation = Situation.GAME;
                 break;
             case Situation.GAME:
                 this.paintGame(ctx);
@@ -169,12 +165,10 @@ class Engine {
         ctx.fillRect(0, 0, Engine.BOARD_WIDTH * Engine.TILE_WIDTH, Engine.BOARD_HEIGHT * Engine.TILE_HEIGHT);
     }
     paintGame(ctx) {
+        this.clearScreen(ctx);
         for (let x = 0; x < Engine.BOARD_WIDTH; x++) {
             for (let y = 0; y < Engine.BOARD_HEIGHT; y++) {
                 switch (this.board.getTileState(x, y)) {
-                    case TileState.EMPTY:
-                        this.drawRect(ctx, x, y, Engine.EMPTY_COLOR);
-                        break;
                     case TileState.RED:
                         this.drawCircle(ctx, x, y, "#ba4747");
                         break;
@@ -191,10 +185,6 @@ class Engine {
             }
         }
     }
-    drawRect(ctx, x, y, color) {
-        ctx.fillStyle = color;
-        ctx.fillRect(x * Engine.TILE_WIDTH, y * Engine.TILE_HEIGHT, x * Engine.TILE_WIDTH + Engine.TILE_WIDTH, y * Engine.TILE_HEIGHT + Engine.TILE_HEIGHT);
-    }
     drawCircle(ctx, x, y, color) {
         ctx.beginPath();
         ctx.fillStyle = color;
@@ -210,8 +200,7 @@ class Engine {
         let canvasPosition = this.getElementPosition(this.canvas);
         let mouseX = event.pageX - canvasPosition.x;
         let mouseY = event.pageY - canvasPosition.y;
-        let tileX = Math.floor(mouseX / Engine.TILE_WIDTH);
-        let tileY = Math.floor(mouseY / Engine.TILE_HEIGHT);
+        this.onStep();
         switch (this.situation) {
             case Situation.GAME:
                 let tileX = Math.floor(mouseX / Engine.TILE_WIDTH);
@@ -220,12 +209,14 @@ class Engine {
                     let removedCount = this.removeSameBalls(tileX, tileY);
                     if (removedCount > 1) {
                         this.situation = Situation.ANIMATION;
+                        this.onStep();
                         this.score += removedCount * removedCount;
                     }
                 }
                 break;
             case Situation.END_GAME:
                 this.initGame();
+                this.onStep();
                 break;
         }
     }
@@ -305,15 +296,10 @@ class Engine {
 Engine.HIGHSCORE_KEY = "ru.urvanov.tscolorballs.highScore";
 Engine.BOARD_WIDTH = 10;
 Engine.BOARD_HEIGHT = 10;
-Engine.TICK_MILLISECONDS = 100;
+Engine.TICK_MILLISECONDS = 80;
 Engine.TILE_WIDTH = 32;
 Engine.TILE_HEIGHT = 32;
 Engine.EMPTY_COLOR = "#404040";
-class GameCanvas {
-    constructor() {
-        this.canvas = document.getElementById("tscolorballscanvas");
-    }
-}
 class Point {
     constructor(x, y) {
         this.x = x;
@@ -329,9 +315,9 @@ var Situation;
 (function (Situation) {
     Situation[Situation["CLEAR_SCREEN"] = 0] = "CLEAR_SCREEN";
     Situation[Situation["GAME"] = 1] = "GAME";
-    Situation[Situation["END_GAME"] = 2] = "END_GAME";
-    Situation[Situation["ANIMATION"] = 3] = "ANIMATION";
-    Situation[Situation["SHOW_SCORE"] = 4] = "SHOW_SCORE";
+    Situation[Situation["ANIMATION"] = 2] = "ANIMATION";
+    Situation[Situation["SHOW_SCORE"] = 3] = "SHOW_SCORE";
+    Situation[Situation["END_GAME"] = 4] = "END_GAME";
 })(Situation || (Situation = {}));
 var TileState;
 (function (TileState) {
@@ -340,10 +326,8 @@ var TileState;
     TileState[TileState["GREEN"] = 2] = "GREEN";
     TileState[TileState["BLUE"] = 3] = "BLUE";
     TileState[TileState["YELLOW"] = 4] = "YELLOW";
-    TileState[TileState["ORANGE"] = 5] = "ORANGE";
-    TileState[TileState["GOLD"] = 6] = "GOLD";
-    TileState[TileState["CYAN"] = 7] = "CYAN";
 })(TileState || (TileState = {}));
+// Created by https://urvanov.ru
 (function () {
     document.addEventListener("DOMContentLoaded", function () {
         let engine = new Engine();
